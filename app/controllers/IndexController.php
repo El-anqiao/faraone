7f81aacfb2bdd71196e9d115eaa0ebc6454c55cc
faraone <El_anqiao@hotmail.com>
2017-09-07 00:17:52 +0800

    添加Beanbun插件抓取网页

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,10 +1,23 @@
 <?php
 
+use Beanbun\Beanbun;
 class IndexController extends ControllerBase
 {
 
     public function indexAction()
     {
+
+
+        $beanbun = new Beanbun;
+        $beanbun->seed = [
+            'http://www.950d.com/',
+            'http://www.950d.com/list-1.html',
+            'http://www.950d.com/list-2.html',
+        ];
+        $beanbun->afterDownloadPage = function($beanbun) {
+            file_put_contents(__DIR__ . '/' . md5($beanbun->url), $beanbun->page);
+        };
+        $beanbun->start();
 
     }
 
